a4e2d48ed061b5ca10b04562a5076510c663a99d
Doug Guzman <dgguzman@mail.bradley.edu>
2021-06-12 19:56:53 -0500

    Implements new art, changes battle queue a bit, next monster occurs.

--- Assets/Scripts/Monsters/EnemyMonsterBehaviour.cs
+++ Assets/Scripts/Monsters/EnemyMonsterBehaviour.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections.Generic;
 using Channels;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace Monsters
 {
@@ -11,20 +13,63 @@ namespace Monsters
 
         [SerializeField]
         private VoidChannel enemyDoActionChannel;
+
+        [SerializeField]
+        private BodyPartChannel enemyBodyChange;
+        
         
         [SerializeField]
-        private List<MONSTER_ACTIONS> actionList = new List<MONSTER_ACTIONS>();
+        private Queue<MONSTER_ACTIONS> actionList = new Queue<MONSTER_ACTIONS>();
 
+        [SerializeField] private VoidChannel nextMonster;
+
+        [SerializeField] private List<BodyPartBaseSO> enemyTwo = new List<BodyPartBaseSO>();
+
+        [SerializeField] private List<BodyPartBaseSO> enemyBoss = new List<BodyPartBaseSO>();
+
+        private List<List<BodyPartBaseSO>> enemies = new List<List<BodyPartBaseSO>>();
+
+        private int counter = 1;
         public void GenerateAction()
         {
             MONSTER_ACTIONS newAction = (MONSTER_ACTIONS)Random.Range(0, (int) MONSTER_ACTIONS.STALL);
-            actionList.Add(newAction);
+            actionList.Enqueue(newAction);
             enemyQueueActionChannel.RaiseEvent(newAction);
         }
 
         public void DoAction()
         {
             enemyDoActionChannel.RaiseEvent();
+            actionList.Dequeue();
+            if (actionList.Count <= 2)
+            {
+                GenerateAction();
+            }
+
+        }
+
+        private void OnEnable()
+        {
+            nextMonster.OnEventRaised += NextMonster;
+        }
+
+        private void OnDisable()
+        {
+            nextMonster.OnEventRaised -= NextMonster;
+        }
+
+        private void NextMonster()
+        {
+            List<BodyPartBaseSO> currentEnemy = enemies[counter];
+            for(int i = 0; i < currentEnemy.Count; ++i)
+            {
+                BodyPartChangeEvent newPart = new BodyPartChangeEvent();
+                newPart.location = (PART_LOCATIONS)i;
+                newPart.newPart = currentEnemy[i];
+                enemyBodyChange.RaiseEvent(newPart);
+            }
+
+            ++counter;
         }
 
         private void Start()
@@ -33,6 +78,9 @@ namespace Monsters
             {
                 enemyQueueActionChannel.RaiseEvent(monsterAction);
             }
+            
+            enemies.Add(enemyTwo);
+            enemies.Add(enemyBoss);
         }
         
     }
